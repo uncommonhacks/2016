685167aa21832aaaaf2a3a631ecb5fd09309d70a
Stephen Huh <stephenhuh@gmail.com>
2015-09-20 07:59:00 -0400

    get typeform to work with express

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+//unique routing for typeform
 router.get('/apply', function(req, res, next) {
-	res.render('typeform');
+	res.sendFile(path.join(__dirname, '../views', 'typeform.html'));
 });
 
 module.exports = router;
